e5d62ba1cef6f16759380abaa02c6bbd953e2170
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-11-23 19:55:39 +0000

    Added example implementation unit test

---
+++ implementation/src/test/kotlin/io/github/tomplum/aoc/ExampleImplementationTest.kt
@@ -0,0 +1,16 @@
+package io.github.tomplum.aoc
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import org.junit.jupiter.api.Test
+
+class ExampleImplementationTest {
+
+    private val implementation = ExampleImplementation()
+
+    @Test
+    fun shouldReturnTwelve() {
+        val solution = implementation.solveSomeProblem()
+        assertThat(solution).isEqualTo(12)
+    }
+}
