3254efb32af807a197458b5f0b2fdd0b8f89f3b2
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2024-12-03 20:10:44 +0000

    Setup | First pass of bash script to replace placeholders

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+  echo "Make sure you pass the current year: $0 <current_year>"
+  exit 1
+fi
+
+# Update the years in the modules paths in the IntelliJ run XML implementationRunConfigFilePaths
+yearPlaceholder="{YEAR}"
+currentYear=$1
+
+# Update the implementation unit tests run config
+implementationRunConfigFilePath="./.run/Implementation Tests.run.xml"
+sed -i.bak "s|$yearPlaceholder|$currentYear|g" "$implementationRunConfigFilePath"
+
+# Update the solutions unit tests run config
+solutionTestsRunConfigFilePath="./.run/Solution Tests.run.xml"
+sed -i.bak "s|$yearPlaceholder|$currentYear|g" "$solutionTestsRunConfigFilePath"
+
+# Update the solutions run config
+solutionsRunConfigFilePath="./.run/Run Solutions.run.xml"
+sed -i.bak "s|$yearPlaceholder|$currentYear|g" "$solutionsRunConfigFilePath"
